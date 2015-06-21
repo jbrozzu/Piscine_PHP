d97184859b52e01419e4017d7c23909b2c64fa42
Jeremie BROZZU <jbrozzu@e2r12p23.42.fr>
2015-06-21 23:00:06 +0200

    plop

--- chupa/htdocs/index.php
+++ chupa/htdocs/index.php
@@ -12,67 +12,58 @@
 
 	<header class="box">
 		<img id="logo" src="/img/logo.png"></img>
-		<h1 class="fontgrad">Chupa Chups !</h1>
+		<img id="chupa-title" src="/img/chupa.png">
 
 	<div class="form_login">
-		<form method="POST" action="login.php"> 
-			Identifiant: <input type="text" name="login" placeholder="Login" value="" />
+		<form method="POST" action="login.php" >
+			Identifiant: <input type="text" class="css-input" name="login" placeholder="Login" value="" />
 
-			Mot de passe: <input type="password" name="passwd" placeholder="Mot de passe" value="" />
+			Mot de passe: <input type="password" class="css-input" name="passwd" placeholder="Mot de passe" value="" />
 			<input type="submit" name="submit" value="OK"/>
 		</br>
-			<a href="create.html">Créer un compte</a>
+			
 		</form>
+		<a class="button2" href="create.html" >Créer un compte</a>
 	</div>
-
-
 			<div class="buttons">
-				<a href="
-							
-					<?PHP
-							if ($_SESSION['loggued_on_user'] != "")
-							{ 
-					?>
-								login.html
-					<?PHP
-							}
-							else
-							{
-					?>
-								index.php
-					<?PHP
-							}
-					?>
-
-
-				 " class="button"/>Accueil</a>
-				<a href="#" class="button"/>Boutique</a>
+				<a href="index.php" class="button"/>Accueil</a>
+				<a href="store_nolog.html" class="button"/>Boutique</a>
 				<a href="#" class="button"/>Panier</a>
 				<!-- <a href="#" class="button"/>Login</a> -->
 			</div>
 
 	</header>
-			<div class="container"/>
-				<p id="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-						unknown printer took a galley of type and scrambled it to make a type specimen book. 
-						It has survived not only five centuries, but also the leap into electronic typesetting, 
-						remaining essentially unchanged. It was popularised in the 1960s with the release of 
-						Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-						software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			<div class="container" style="font-family: Comic Sans MS; font-size:14pt; font-weight: bold;"/>
+				<p>Chupa Chups est une marque de sucettes cr&eacute&eacutee en 1958 par le catalan Enric Bernat, 
+					un espagnol qui voulait cr&eacuteer un bonbon qui ne salit pas les doigts. Il monta ainsi 
+					la petite boule de sucre sur un petit b&acirctonnet et la nomma Chupa Chups (suce la sucette). 
+					Cependant, Enric Bernat la nomma en premier GOL pour sa ressemblance avec un ballon 
+					de football et la bouche comme une cage de but. Mais n'&eacutetant pas satisfait de ce nom, 
+					il engagea une agence publicitaire pour donner un nom plus accrocheur.</p>
+
+				<p>La cr&eacuteation du logo fut confi&eacutee au c&eacutel&egravebre artiste Salvador Dali en 1969. Le premier terme 
+					" Chupa " vient du verbe chupar qui signifie sucer et Chups symbolise le bruit de la sucette 
+					sortant de la bouche.</p>
+
+				<p>Pour commercialiser sa sucette, une chanson fut cr&eacute&eacutee dont le refrain disait chupa chupa chupa 
+					un chups (en fran&ccedilais " suce suce suce une chups ").</p>
+
+				<p>En 2004, Chupa Chups a am&eacutelior&eacute la recette des Classic en rajoutant de la pulpe de fruits 
+					rendant ainsi les sucettes plus savoureuses. Toutes les sucettes Classic sont &agrave 0% de mati&egravere 
+					grasse et contiennent des colorants d'origine naturelle.</p>
+
+				<p>Chupa Chups a f&ecirct&eacute son cinquanti&egraveme anniversaire en 2008. Pour cette occasion la marque ressort 
+					ses tout premiers parfums : cola, fraise, orange, citron, vanille, choco-vanille, et fraise-cr&egraveme.</p>
 
 
 			
-				<iframe width="854" height="510" src="https://www.youtube.com/embed/HzG3XGYTgvQ" frameborder="0" allowfullscreen></iframe>
+				<iframe style="margin:auto;" width="854" height="510" src="https://www.youtube.com/embed/HzG3XGYTgvQ" frameborder="0" allowfullscreen></iframe>
+
+				<img id="chupa-end" src="/img/chupa-lo.png">
+
+				<a class="button3" href="http://local.42.fr:8080/phpmyadmin/" >Accès Admin</a>
 			</div>
 
-	
-	
-
-
-	<footer>
-		<p>plop</p1>
-	</footer>
 
 
 </div>
@@ -81,6 +72,3 @@
 
 </body>
 </html>
-
-
-<!-- <a <head></head>ref="phpinfo.php">PHP info</a></body></html> -->
---
+++ chupa/htdocs/store.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="chupa.css">
+		<link rel="icon" type="image/png" href="/img/logo.png">
+		<title>Chupa Chups !</title>
+	</head>
+	<body bgcolor="#A69256">
+
+<div class ="general">
+	
+	<header class="box">
+		<img id="logo" src="/img/logo.png"></img>
+		<img id="chupa-title" src="/img/chupa.png">
+
+	
+			<div class="buttons4">
+				<a href="login.html" class="button"/>Accueil</a>
+				<a href="#" class="button"/>Boutique</a>
+				<a href="#" class="button"/>Panier</a>
+				<a href="logout.php" class="button"/>Logout</a>
+			</div>
+
+	</header>
+	<div class="container">
+		<p id="product">Notre selection de produit :</p>
+		<div class="NAME1" style="float:left;">MAGICS</div> <div class="NAME2" style="float:left;">FRUIT-TELLA</div> <div class="NAME3" style="float:left;">BUBBLE GUM</div> <div class="NAME4" style="float:left;">THE BEST OF</div>
+		<ul>
+			<div id="ligne1">
+<div id="ligne1">
+			<li><img src="/img/magic_paq.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/fruit_paq.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/gum_paq.gif"/><a href="#" class="btn">Acheter</a></li>  
+			<li><img src="/img/bestof_paq.gif"/><a href="#" class="btn">Acheter</a></li></div>
+
+			<div class="PRICE1" style="float:left;">5,80&euro; </div> <div class="PRICE2" style="float:left;">5,60&euro; </div> <div class="PRICE3" style="float:left;">5,30&euro; </div> <div class="PRICE4" style="float:left;">6,10&euro; </div>
+
+	<div class="NAME5" style="float:left;">STAND MAGICS</div> <div class="NAME6" style="float:left;">STAND FRUIT-TELLA</div> <div class="NAME7" style="float:left;">STAND BUBBLE GUM</div> <div class="NAME8" style="float:left;">STAND THE BEST OF</div>
+		
+<div id="ligne2">
+			<li><img src="/img/magic_stand.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/fruit_stand.gif"/><a href="#" class="btn">Acheter</a></li> 
+			<li><img src="/img/gum_stand.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/bestof_stand.gif"/><a href="#" class="btn">Acheter</a></li></div>
+
+			<div class="PRICE5" style="float:left;">15,80&euro; </div> <div class="PRICE6" style="float:left;">14,60&euro; </div> <div class="PRICE7" style="float:left;">15,30&euro; </div> <div class="PRICE8" style="float:left;">18,10&euro; </div>
+
+		<div class="NAME9" style="float:left;">BOITE MAGICS</div> <div class="NAME10" style="float:left;">BOITE FRUIT-TELLA</div> <div class="NAME11" style="float:left;">BOITE BUBBLE GUM</div> <div class="NAME12" style="float:left;">BOITE THE BEST OF</div>
+
+<div id="ligne3">
+			<li><img src="/img/magic_box.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/fruit_box.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/gum_box.gif"/><a href="#" class="btn">Acheter</a></li>
+			<li><img src="/img/bestof_box.gif"/><a href="#" class="btn">Acheter</a></li></div>
+
+			<div class="PRICE9" style="float:left;">13,80&euro; </div> <div class="PRICE10" style="float:left;">12,60&euro; </div> <div class="PRICE11" style="float:left;">12,30&euro; </div> <div class="PRICE12" style="float:left;">14,10&euro; </div>
+			
+		</ul>
+	</div>
+</div>
+	<!-- <footer>
+		<p>plop</p1>
+	</footer> -->
+	</body>
+</html>
